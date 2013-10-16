0801d25d8170da17b988a3ec56cc1673ea635a38
Javier A. Ortiz Bultron <javydreamercsw@bitbucket.com>
2013-10-16 10:15:48 -0500

    Refactor a default showPairing method to abstracttournament.

--- Elimination-Tournament/src/main/java/net/sourceforge/javydreamercsw/tournament/manager/elimination/tournament/Elimination.java
+++ Elimination-Tournament/src/main/java/net/sourceforge/javydreamercsw/tournament/manager/elimination/tournament/Elimination.java
@@ -204,18 +204,6 @@ public class Elimination extends AbstractTournament
     }
 
     @Override
-    public void showPairings() {
-        Map<Integer, Encounter> pairings = getPairings();
-        for (Entry<Integer, Encounter> entry : pairings.entrySet()) {
-            LOG.info(MessageFormat.format("{0} vs. {1}",
-                    entry.getValue().getEncounterSummary().keySet().toArray(
-                            new TeamInterface[]{})[0].getTeamMembers().get(0).toString(),
-                    entry.getValue().getEncounterSummary().keySet().toArray(
-                            new TeamInterface[]{})[1].getTeamMembers().get(0).toString()));
-        }
-    }
-
-    @Override
     public int getMinimumAmountOfRounds() {
         /**
          * Assuming two competitors per match, if there are n competitors, there
