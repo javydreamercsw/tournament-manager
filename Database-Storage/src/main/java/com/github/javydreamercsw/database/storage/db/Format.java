5982d67ceac9d5d191e38f6f745f9fe0fa2b2b8d
Javier Ortiz <javierortiz@pingidentity.com>
2019-01-04 10:42:52 -0600

    Remove duplicate methods.

--- Database-Storage/src/main/java/com/github/javydreamercsw/database/storage/db/Format.java
+++ Database-Storage/src/main/java/com/github/javydreamercsw/database/storage/db/Format.java
@@ -179,24 +179,4 @@ public class Format implements Serializable
   {
     this.description = description;
   }
-
-  public String getName()
-  {
-    return name;
-  }
-
-  public void setName(String name)
-  {
-    this.name = name;
-  }
-
-  public String getDescription()
-  {
-    return description;
-  }
-
-  public void setDescription(String description)
-  {
-    this.description = description;
-  }
 }
